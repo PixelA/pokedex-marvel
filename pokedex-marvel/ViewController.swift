49b205216d547da1b874125624072b95ba1037c5
Asherlee Levy <asherleelevy@Asherlees-MacBook-Pro.local>
2016-08-12 19:55:46 -0400

    Added Audio Player with music file . Display button to turn on and off the music

--- pokedex-marvel/ViewController.swift
+++ pokedex-marvel/ViewController.swift
@@ -9,21 +9,47 @@
 import UIKit
 import AVFoundation
 
-class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource,UICollectionViewDelegateFlowLayout {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource,UICollectionViewDelegateFlowLayout, UISearchBarDelegate {
     
     
     @IBOutlet weak var collection: UICollectionView!
     
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    
     var pokemon = [Pokemon]()
+    var filteredMode = [Pokemon]()
     var musicPlayer: AVAudioPlayer!
+    var inSearchMode = false
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         collection.delegate = self
         collection.dataSource = self
+        searchBar.delegate = self
+
         
+        initAudio()
         parsePokemonCSV()
+        
+    }
+    
+    func initAudio() {
+        
+        let path = NSBundle.mainBundle().pathForResource("music", ofType: "mp3")!
+        
+        do {
+            musicPlayer = try AVAudioPlayer(contentsOfURL: NSURL(string: path)!)
+            musicPlayer.prepareToPlay()
+            musicPlayer.numberOfLoops = -1
+            musicPlayer.play()
+            
+            
+        } catch let err as NSError {
+            print(err.debugDescription)
+        }
         
     }
     
@@ -75,9 +101,21 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         
         return CGSizeMake(105, 105)
     }
-    @IBAction func musicBtnPressed(sender: AnyObject) {
+    @IBAction func musicBtnPressed(sender: UIButton!) {
+        
+        if musicPlayer.playing {
+            musicPlayer.stop()
+            sender.alpha = 0.2
+    
+        } else {
+            musicPlayer.play()
+            sender.alpha = 1.0
+        }
     }
  
+    func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
+        <#code#>
+    }
 }
 
 
